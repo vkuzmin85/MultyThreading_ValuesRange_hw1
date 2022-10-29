7b7f82c28c1bf4507285f63c9562296a080097eb
vkuzmin85 <vpkhome@gmail.com>
2022-10-29 19:45:34 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.*;
+
+public class Main {
+    public static void processRange(String text) {
+        int maxSize = 0;
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (i >= j) {
+                    continue;
+                }
+                boolean bFound = false;
+                for (int k = i; k < j; k++) {
+                    if (text.charAt(k) == 'b') {
+                        bFound = true;
+                        break;
+                    }
+                }
+                if (!bFound && maxSize < j - i) {
+                    maxSize = j - i;
+                }
+            }
+        }
+        System.out.println(text.substring(0, 100) + " -> " + maxSize);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+        List<Thread> threads = new ArrayList<>();
+        for (String str : texts) {
+            threads.add(new Thread(() -> {
+                processRange(str);
+            }));
+        }
+        long startTs = System.currentTimeMillis(); // start time
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+        }
+        long endTs = System.currentTimeMillis(); // end time
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
